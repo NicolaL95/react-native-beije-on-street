5ed2635b0de6b4e1f295389d05b81a49ca04a066
NicolaL95 <nicola.lilla@gmail.com>
2022-06-13 11:43:22 +0200

    add menu navigation

--- screens/menu/MenuPage.tsx
+++ screens/menu/MenuPage.tsx
@@ -1,9 +1,36 @@
-import React, { FC } from 'react'
-import { View, Text } from 'react-native'
-const MenuPage: FC = () => {
+import React, { FC, ReactElement } from 'react'
+import { View, Text, Pressable } from 'react-native'
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+type RootStackParamList = {
+    Tutorial: undefined,
+    DrawPage: undefined,
+    Gallery: undefined,
+}
+
+type Props = NativeStackScreenProps<RootStackParamList>;
+
+const MenuPage: FC<Props> = ({ navigation }): ReactElement => {
     return (
         <View>
-            <Text>MenuPage</Text>
+            <Text>Sketch onStreet</Text>
+            <View>
+                <Pressable
+                    onPress={() => { navigation.navigate('DrawPage') }}>
+                    <Text>Disegna</Text>
+                </Pressable>
+            </View>
+            <View>
+                <Pressable
+                    onPress={() => { navigation.navigate('Gallery') }}>
+                    <Text>Galleria</Text>
+                </Pressable>
+            </View>
+            <View>
+                <Pressable
+                    onPress={() => { navigation.navigate('Tutorial') }}>
+                    <Text>Tutorial</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
